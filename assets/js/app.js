5fde9d7dd1f0b2145f7c72c30733a1c17e6048a6
Bipul38 <optimisticbipul@gmail.com>
2024-01-19 14:22:42 +0600

    js updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,107 @@
+var product_amount = 10;
+var header_cart_products = [
+    {
+        image_no: 3,
+        name: `MacBook Pro M2 MNEJ3 2022 LLA 
+        13.3 inch`,
+        price: `999`,
+    },
+    {
+        image_no: 5,
+        name: `Inateck 12.3-13 Inch MacBook Case Sleeve `,
+        price: `25`,
+    },
+    {
+        image_no: 7,
+        name: `Laptop Privacy Screen for 13 inch 
+        MacBook Pro & MacBook Air`,
+        price: `50`,
+    },
+];
+var jumbo_menu_products = [
+    {
+        image_no: `./assets/images/dummy_products/1.jpg`,
+        name: `Extra Layer for Travellers`,
+        price: `250`,
+    },
+    {
+        image_no: `./assets/images/dummy_products/3.jpg`,
+        name: `MacBook Pro M2 MNEJ3 2022 LLA 
+        13.3 inch`,
+        price: `999`,
+    },
+    {
+        image_no: `./assets/images/dummy_products/5.jpg`,
+        name: `Inateck 12.3-13 Inch MacBook Case Sleeve `,
+        price: `25`,
+    },
+    {
+        image_no: `./assets/images/dummy_products/7.jpg`,
+        name: `Laptop Privacy Screen for 13 inch 
+        MacBook Pro & MacBook Air`,
+        price: `50`,
+    },
+   
+];
+
+var product_categories = [
+    {
+        icon: `./assets/images/icons/mobile.svg`,
+        name: `Mobile Phones`,
+    },
+    {
+        icon: `./assets/images/icons/laptop.svg`,
+        name: `Laptops & Computers`,
+    },  
+    {
+        icon: `./assets/images/icons/tablet.svg`,
+        name: `Tablets & E-reader`,
+    },  
+    {
+        icon: `./assets/images/icons/wearables.svg`,
+        name: `Wearables`,
+    },  
+    {
+        icon: `./assets/images/icons/wearables.svg`,
+        name: `Audio`,
+    },
+    {
+        icon: `./assets/images/icons/wearables.svg`,
+        name: `Cameras`,
+    },
+    {
+        icon: `./assets/images/icons/wearables.svg`,
+        name: `Gaming`,
+    },
+    {
+        icon: `./assets/images/icons/wearables.svg`,
+        name: `Networking`,
+    },
+    {
+        icon: `./assets/images/icons/accessories.svg`,
+        name: `Accessories`,
+    },
+    {
+        icon: `./assets/images/icons/wearables.svg`,
+        name: `Smart Phones`,
+    },
+];
+
+var product_subcategories = [
+    {
+        icon: `./assets/images/icons/smartphones.svg`,
+        name: `Smart Phones`,
+    },
+    {
+        icon: `./assets/images/icons/accessories.svg`,
+        name: `accessories`,
+    },
+    {
+        
+        name: `View all`,
+    },
+];
+
 function increment_cart(price_target){
     event.preventDefault();
     let input = event.currentTarget.previousElementSibling;
